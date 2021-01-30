543f648071e387963994d4f634c4d940f71dad3f
jackjackE <tmddjs210@naver.com>
2021-01-30 23:03:17 +0900

    [Delete] Koin 모듈들 제거

--- AndroidArchitectureStudy/presentation/src/main/java/com/mtjin/presentation/module/NetworkModule.kt
+++
@@ -1,9 +0,0 @@
-package com.mtjin.presentation.module
-
-import com.mtjin.presentation.utils.NetworkManager
-import org.koin.core.module.Module
-import org.koin.dsl.module
-
-val networkModule: Module = module {
-    single { NetworkManager(get()) }
-}
